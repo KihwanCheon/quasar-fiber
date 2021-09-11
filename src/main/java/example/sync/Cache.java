7555547b4589120051f64a9a82db0edea6e5138f
KihwanCheon <gujjy97@gmail.com>
2021-09-11 18:05:25 +0900

    Add quasar example, jmh example

---
+++ src/main/java/example/sync/Cache.java
@@ -0,0 +1,19 @@
+package example.sync;
+
+import java.util.Collection;
+import java.util.Map;
+
+public interface Cache <K, V> {
+
+    int size();
+
+    Map<K, V> clear();
+
+    void put(K k, V v);
+
+    V remove(K k);
+
+    V value(K key);
+
+    Collection<K> keys();
+}
---
+++ src/main/java/example/sync/CacheSynchronized.java
@@ -0,0 +1,55 @@
+package example.sync;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+import java.util.function.Supplier;
+
+@Slf4j
+public class CacheSynchronized<K, V> implements Cache<K, V> {
+
+    Map<K, V> container = new HashMap<>();
+
+    synchronized <T> T sync(Supplier<T> supplier) {
+        return supplier.get();
+    }
+
+
+    @Override
+    public int size() {
+        return sync(() -> container.size());
+    }
+
+    @Override
+    public Map<K, V> clear() {
+        log.trace("clear");
+        return sync(() -> new HashMap<>(container));
+    }
+
+    @Override
+    public void put(K k, V v) {
+        log.trace("put");
+        sync(() -> {
+            container.put(k, v);
+            return true;
+        });
+    }
+
+    @Override
+    public V remove(K k) {
+        log.trace("remove");
+        return sync(() -> container.remove(k));
+    }
+
+    @Override
+    public V value(K key) {
+        log.trace("value: {}", key);
+        return sync(() -> container.get(key));
+    }
+
+    @Override
+    public Collection<K> keys() {
+        log.trace("keys");
+        return sync(() -> new ArrayList<>(container.keySet()));
+    }
+}

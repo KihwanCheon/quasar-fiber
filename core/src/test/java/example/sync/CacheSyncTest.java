e40488fdde1ea2b42a797cbd8c0b96684827a7d5
KihwanCheon <gujjy97@gmail.com>
2021-09-11 20:17:59 +0900

    Modularize jmh

--- core/src/test/java/example/sync/CacheSyncTest.java
+++ core/src/test/java/example/sync/CacheSyncTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 class CacheSyncTest {
